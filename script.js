a11cbb03dfd0094ea7be7cb319c76b1854f3ed8a
unknown <rjd.atx@gmail.com>
2021-07-23 18:13:32 -0500

    async await approach

--- script.js
+++ script.js
@@ -4,17 +4,31 @@ const jokeButton = document.getElementById(`jokeBtn`);
 jokeButton.addEventListener(`click`, generateJoke);
 generateJoke();
 
-// Non-async approach
-function generateJoke() {
+async function generateJoke() {
 	const config = {
 		headers: {
 			Accept: `application/json`,
 		},
 	};
 
-	fetch(`https://icanhazdadjoke.com/`, config)
-		.then((res) => res.json())
-		.then((data) => {
-			jokeElement.innerHTML = data.joke;
-		});
+	const response = await fetch(`https://icanhazdadjoke.com/`, config);
+	const data = await response.json();
+
+	jokeElement.innerHTML = data.joke;
 }
+
+// Non-async approach
+//
+// function generateJoke() {
+// 	const config = {
+// 		headers: {
+// 			Accept: `application/json`,
+// 		},
+// 	};
+
+// 	fetch(`https://icanhazdadjoke.com/`, config)
+// 		.then((res) => res.json())
+// 		.then((data) => {
+// 			jokeElement.innerHTML = data.joke;
+// 		});
+// }
